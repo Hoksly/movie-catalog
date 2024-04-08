1a97df3fcf996aaa05b1901bcc0021660218d651
hoksly <realhoksly@gmail.com>
2024-04-08 11:16:15 +0300

    model update, proper naming added

--- MovieCatalog/Models/Category.cs
+++ MovieCatalog/Models/Category.cs
@@ -1,7 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace MovieCatalog.Models
 {
+    [Table("categories")]   
     public class Category
     {
-        
+        [Key]
+        [Column("Id")]
+        public int Id { get; set; } // Primary Key
+
+        [StringLength(200)]
+        [Column("name")]
+        public string Name { get; set; }
+        [Column("parent_category_id")]
+        public int? ParentCategoryId { get; set; } // nullable foreign key
+
+        public ICollection<FilmCategory> FilmCategories { get; set; } // Navigation property for many-to-many relationship
     }
+
 }
