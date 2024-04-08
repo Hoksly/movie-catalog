0098d8f7ced45a0ca7ae62350f6ef2928fa26924
hoksly <realhoksly@gmail.com>
2024-04-08 11:20:09 +0300

    not finished categories controller added

---
+++ MovieCatalog/Controllers/CategoriesController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MovieCatalog.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
