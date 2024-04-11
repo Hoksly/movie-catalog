454a54fc44189ca31872556a38de7b739d109bcb
hoksly <realhoksly@gmail.com>
2024-04-11 21:44:10 +0300

    Category view model adjustments

--- MovieCatalog/Models/CategoryViewModel.cs
+++ MovieCatalog/Models/CategoryViewModel.cs
@@ -4,19 +4,17 @@ namespace MovieCatalog.Models
     {
         public Category Category {get; set;} = category;
         public List<CategoryViewModel> SubCategories { get; set; } = new List<CategoryViewModel>();
+        public List<Film> Films { get; set; } = new List<Film>();
         public int NestingLevel { get; set; } = 0;
 
         public void calculateNestingLevel()
         {
-            if (Category.ParentCategoryId != null)
+            foreach (var subCategory in SubCategories)
             {
-                
-                foreach (var subCategory in SubCategories)
-                {
-                    subCategory.calculateNestingLevel();
-                    NestingLevel = int.Max(NestingLevel, subCategory.NestingLevel + 1);
-                }
+                subCategory.calculateNestingLevel();
+                NestingLevel = int.Max(NestingLevel, subCategory.NestingLevel + 1);
             }
+            
         }
     }
 }
